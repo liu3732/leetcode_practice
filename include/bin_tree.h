1d632f4dadc29edd6b92c268dc7666be05aab8a0
liu3732 <liu3732@gmail.com>
2023-10-26 16:09:01 +0800

    104. 二叉树的层次

---
+++ include/bin_tree.h
@@ -0,0 +1,16 @@
+#ifndef __TREE__
+#define __TREE__
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+#define NULL_NODE __INT32_MAX__
+
+struct TreeNode *PreArrayToTree(int **p2a, unsigned int *p2num_size);
+struct TreeNode *PostArrayToTree(int **p2a, unsigned int *p2num_size);
+struct TreeNode *LayerArrayToTree(int **p2a, unsigned int *p2num_size);
+
+#endif
---
+++ max_depth_104.c
@@ -0,0 +1,39 @@
+/*
+ * @Author: liu3732 liu3732@gmail.com
+ * @Date: 2023-10-26 09:42:55
+ * @LastEditors: liu3732 liu3732@gmail.com
+ * @LastEditTime: 2023-10-26 16:05:03
+ * @FilePath: \leetcode_practice\max_depth_104.c
+ * @Description: 
+ * 
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
+ */
+#include <bin_tree.h>
+#include <stdlib.h>
+#include <assert.h>
+
+static inline int max_int(int a, int b)
+{
+    return a > b ? a : b;
+}
+
+int maxDepth(struct TreeNode *root)
+{
+    if (root == NULL) {
+        return 0;
+    }
+    return (max_int(maxDepth(root->left), maxDepth(root->right)) + 1);
+}
+
+
+int main()
+{
+    int a[] = {3, NULL_NODE, NULL_NODE};
+    int len = sizeof(a)/sizeof(int);
+    
+    int *pa = a;
+    struct TreeNode *root = PreArrayToTree(&pa, &len);
+    assert(maxDepth(root) == 1);
+    printf("test has been passed.\n");
+    return 0;
+}
